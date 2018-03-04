d12adbe8a7f2bf9809612c1b2e680058fb4cde85
voxelion <22245604+Voxelion@users.noreply.github.com>
2018-03-04 17:43:32 +0100

    pygame dziala

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import pygame
-print("helloworld")
-print()
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+display_surface = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("atlanta-city")
+
+while True: #main game loop
+    for event in pygame.event.get(): #nasluchiwanie
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
