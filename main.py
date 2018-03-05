08dc9d0b13589c527a9c4158de718a8385a67681
voxelion <22245604+Voxelion@users.noreply.github.com>
2018-03-05 18:42:58 +0100

    dziala :V

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import pygame, sys
 from pygame.locals import *
-pygame.init()
-display_surface = pygame.display.set_mode((1280, 720))
+
+
+screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("atlanta-city")
+
+background = pygame.image.load("default.png").convert()
+car = pygame.image.load("car.png").convert_alpha()
+
+Px = 640
+Py = 360
+Cx = 0
+Cy = 0
+
 
 while True: #main game loop
     for event in pygame.event.get(): #nasluchiwanie
         if event.type == QUIT:
-            pygame.quit()
+            quit()
             sys.exit()
+
+    Pvx = 0
+    Pvy = 0
+    keys = pygame.key.get_pressed()  #checking pressed keys
+    if keys[K_w]:
+        Pvy -= 1
+    if keys[K_s]:
+        Pvy += 1
+    if keys[K_d]:
+        Pvx += 1
+    if keys[K_a]:
+        Pvx -= 1
+
+    Cx -= Pvx
+    Cy -= Pvy
+    screen.blit(background, (Cx,Cy))
+    screen.blit(car, (Px, Py))
+
+
+    
     pygame.display.update()
